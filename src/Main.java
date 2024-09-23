63510c3b9beedf7937e4e777b74edc61ffb67e27
schelihaeva <146697334+schelihaeva@users.noreply.github.com>
2024-09-23 09:14:31 +0300

    Update Main.java
    sem1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Старт программы");
+        List<Integer> numbers1 = Arrays.asList(5, 8, 7, 21, -11, -52, 0, 89, 4, 8, 9, 46, 45);
+        List<Integer> numbers2 = Arrays.asList(9, 8, -2, 7, 6, 89); // проверка списка без четных чисел
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        OptionalDouble average = numbers1.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .average();
+
+        if (average.isPresent()) {
+            System.out.println("Среднее значение всех четных чисел в списке: " + average.getAsDouble());
+        } else {
+            System.out.println("В списке нет четных чисел.");
         }
+
     }
 }
